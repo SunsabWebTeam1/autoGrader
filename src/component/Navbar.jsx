25af11a17370534d7af8995e663da1d6088a3753
martin <martin.contreras.0302.8@gmail.com>
2024-05-27 22:12:49 -0600

    navbar is fixed
    added assignment service and courses service for firestore

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -6,8 +6,7 @@ import { findStudent, findTeacher } from "../services/AccountService";
 
 function Navbar() {
   const { logOut, user } = UserAuth();
-  const [isTeacher, setIsTeacher] = useState(false);
-  const [isStudent, setIsStudent] = useState(false);
+  const [userType, setUserType] = useState(null);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -17,13 +16,17 @@ function Navbar() {
 
           const studentResult = await findStudent(userId);
           if (studentResult.found) {
-            setIsStudent(true);
+            setUserType("student");
           } else {
             const teacherResult = await findTeacher(userId);
             if (teacherResult.found) {
-              setIsTeacher(true);
+              setUserType("teacher");
+            } else {
+              setUserType("authenticated");
             }
           }
+        } else {
+          setUserType("unauthenticated");
         }
       } catch (error) {
         console.error("Error fetching data:", error);
@@ -33,40 +36,65 @@ function Navbar() {
     fetchData();
   }, [user]);
 
-  return (
-    <AppBar position="static" sx={{ backgroundColor: "#212322" }}>
-      <Toolbar>
-        <img alt="Logo" style={{ height: "50px", marginRight: "auto" }} />
-        {!user && (
-          <>
-            <Typography variant="h6" sx={{ marginLeft: "20px" }}>
+  const renderNavbar = () => {
+    if (!user) {
+      return (
+        <>
+          <Typography variant="h6" sx={{ marginLeft: "20px" }}>
             <Link to="/landingpage" style={{ textDecoration: "none", color: "inherit" }}>Home</Link>
-            </Typography>
-            <Typography variant="h6" sx={{ marginLeft: "20px" }}>
-              <Link to="/loginpage" style={{ textDecoration: "none", color: "inherit" }}>Log In</Link>
-            </Typography>
-            <Typography variant="h6" sx={{ marginLeft: "20px" }}>
-              <Link to="/signuppage" style={{ textDecoration: "none", color: "inherit" }}>Sign Up</Link>
-            </Typography>
-          </>
-        )}
-        {user && (
+          </Typography>
+          <Typography variant="h6" sx={{ marginLeft: "20px" }}>
+            <Link to="/loginpage" style={{ textDecoration: "none", color: "inherit" }}>Log In</Link>
+          </Typography>
+          <Typography variant="h6" sx={{ marginLeft: "20px" }}>
+            <Link to="/signuppage" style={{ textDecoration: "none", color: "inherit" }}>Sign Up</Link>
+          </Typography>
+        </>
+      );
+    }
+  
+    switch (userType) {
+      case "student":
+        return (
           <>
-            {isStudent && (
-              <Typography variant="h6" sx={{ marginLeft: "20px" }}>
-                <Link to={`/homepage/student/${user.uid}`} style={{ textDecoration: "none", color: "inherit" }}>Homepage</Link>
-              </Typography>
-            )}
-            {isTeacher && (
-              <Typography variant="h6" sx={{ marginLeft: "20px" }}>
-                <Link to={`/homepage/teacher/${user.uid}`} style={{ textDecoration: "none", color: "inherit" }}>Homepage</Link>
-              </Typography>
-            )}
+            <Typography variant="h6" sx={{ marginLeft: "20px" }}>
+              <Link to={`/homepage/student/${user.uid}`} style={{ textDecoration: "none", color: "inherit" }}>Homepage</Link>
+            </Typography>
             <Typography variant="h6" sx={{ marginLeft: "20px" }}>
               <Link to="/" style={{ textDecoration: "none", color: "inherit" }} onClick={logOut}>Logout</Link>
             </Typography>
           </>
-        )}
+        );
+      case "teacher":
+        return (
+          <>
+            <Typography variant="h6" sx={{ marginLeft: "20px" }}>
+              <Link to={`/homepage/teacher/${user.uid}`} style={{ textDecoration: "none", color: "inherit" }}>Homepage</Link>
+            </Typography>
+            <Typography variant="h6" sx={{ marginLeft: "20px" }}>
+              <Link to="/" style={{ textDecoration: "none", color: "inherit" }} onClick={logOut}>Logout</Link>
+            </Typography>
+          </>
+        );
+      case "authenticated":
+        return (
+          <>
+            <Typography variant="h6" sx={{ marginLeft: "20px" }}>
+              <Link to="/" style={{ textDecoration: "none", color: "inherit" }} onClick={logOut}>Logout</Link>
+            </Typography>
+          </>
+        );
+      default:
+        return null; // Return null for unknown userType
+    }
+  };
+  
+
+  return (
+    <AppBar position="static" sx={{ backgroundColor: "#212322" }}>
+      <Toolbar>
+        <img alt="Logo" style={{ height: "50px", marginRight: "auto" }} />
+        {renderNavbar()}
       </Toolbar>
     </AppBar>
   );
---
+++ src/component/user-teacher/upload-assignment/UploadAssignmentpage.jsx
@@ -0,0 +1,10 @@
+import React, { useEffect, useState } from "react";
+import { auth } from "../../firebase/firebase";
+
+function UploadAssignmentpage() {
+    <div className="App">
+        <p>UploadAssignmentpage</p>
+    </div>
+}
+
+export default UploadAssignmentpage;
