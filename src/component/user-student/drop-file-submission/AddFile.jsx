dc808589053ff5e5df9bbfa87f01c54d9fba5fc9
Jhio0 <sorianojhio218@gmail.com>
2024-05-30 19:28:43 -0600

    forntend inital for student side

--- src/component/user-student/drop-file-submission/AddFile.jsx
+++ src/component/user-student/drop-file-submission/AddFile.jsx
@@ -1,18 +1,73 @@
+/* eslint-disable jsx-a11y/alt-text */
 import React from 'react';
 import DropFileInput from './DropFileInput.jsx';
-
+import './studentAssignmentLayout.css'
+//IconsImport
+import infoICON from '../../../assets/icons/Info.png';
+import instructionICON from '../../../assets/icons/Instruction.png';
+import DonwloadICON from '../../../assets/icons/Download.png';
+import SubmisionICON from '../../../assets/icons/Submision.png';
 const AddFile = () => {
     const onFileChange = (files) => {
         console.log(files);
     };
 
     return (
-        <div className='bodyforDropFile'>
-            <div className="box">
-                <h2 className="header">
-                    React drop files input
-                </h2>
-                <DropFileInput onFileChange={(files) => onFileChange(files)} />
+        <div className='flexContainer'>
+            <div className='container'>
+                <div id="navBar" className='NavBarLayout'>
+                    <div className='NavBarLayoutContent'>
+                        Assigment Title
+                    </div>
+                </div>
+                <div id="main">
+                    <div className='bodyforDropFile'>
+                        <div>
+                            <h2 className="header">
+                                React drop files input
+                            </h2>
+                            <DropFileInput onFileChange={(files) => onFileChange(files)} />
+                        </div>
+                    </div>
+                    <div className='Submit-NextLayout'>
+                        <div>Submit</div>
+                    </div>
+                </div>
+                <div id="subNavBar" className='subNavBarLayout'>
+                    <div className='subNavBarContent'>
+                        <div className='subNavBarContentInfo'>Submission</div>
+                    </div>
+                </div>
+                <div id="sideBar" className='sideBarLayout'>
+                    <div className='sideBarContent'>
+                        <hr className='sideBarContentHR'/>
+                        <div className='sideBarDetails'>
+                            <div className='sideBarDetailsINDI'>
+                                <img src={infoICON}/> 
+                                Project Details
+                            </div>
+                        </div>
+                        <div className='sideBarDetails'>
+                            <div className='sideBarDetailsINDI'>
+                                <img src={instructionICON}/> 
+                                Instructions
+                            </div>
+                        </div>
+                        <div className='sideBarDetails'>
+                            <div className='sideBarDetailsINDI'>
+                                <img src={DonwloadICON}/> 
+                                Download
+                            </div>
+                        </div>
+                        <hr className='sideBarContentHR'/>
+                        <div className='sideBarDetails'>
+                            <div className='sideBarDetailsINDI'>
+                                <img src={SubmisionICON}/> 
+                                Submission
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     );
--- src/script/appForPython.py
+++ src/script/appForPython.py
@@ -11,7 +11,7 @@ CORS(app)  # Enable CORS for all routes
 # Configure MySQL connection
 mysql_connection = mysql.connector.connect(
     host='localhost',
-    port='3307',
+    port='3306',
     user='root',
     password='password',
     database='file_storage'
