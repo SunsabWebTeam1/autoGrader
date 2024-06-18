34d8954514fb27425893db43a85aa42544ffaf5d
martin <martin.contreras.0302.8@gmail.com>
2024-06-18 13:59:44 -0600

    added demo to landing page

--- src/component/landingpage/About.jsx
+++ src/component/landingpage/About.jsx
@@ -1,5 +1,5 @@
 import { Container, Typography } from '@mui/material';
-import imgDemo from '../../images/LP-Demo.png';
+import autograderDemo from '../../demo/LP-demo.mp4';
 
 function About() {
     return (
@@ -11,14 +11,19 @@ function About() {
                     </Typography>
                     <div className="content">
                         <p>
-                        Autograder automates the grading of programming assignments through automated execution of JUNIT tests on submitted files. 
-                        Teachers upload the assignment along with the JUNIT tests necessary for grading. Once uploaded, students can access the assignment 
-                        and submit their work. Upon submission, the autograder runs the teacher's JUNIT tests to grade the student's assignment, providing pass/fail 
-                        outcomes based on the results.
+                            Autograder automates the grading of programming assignments through automated execution of JUNIT tests on submitted files. 
+                            Teachers upload the assignment along with the JUNIT tests necessary for grading. Once uploaded, students can access the assignment 
+                            and submit their work. Upon submission, the autograder runs the teacher's JUNIT tests to grade the student's assignment, providing pass/fail 
+                            outcomes based on the results.
                         </p>
                     </div>
                 </div>
-                <img src={imgDemo} alt="Demo" />
+                <div className="demo">
+                    <video controls width="100%" style={{ borderRadius: '8px' }}>
+                        <source src={autograderDemo} type="video/mp4" />
+                        Your browser does not support the video tag.
+                    </video>
+                </div>
             </Container>
         </section>
     );
